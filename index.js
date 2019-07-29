aa2803302a62b919aad84eb9b309d39aa69ad988
Jesus Vazquez <jesusvazquez@MacBook-Air-de-Jesus.local>
2019-07-29 15:19:27 -0500

    palabras

---
+++ index.js
@@ -0,0 +1,38 @@
+var hola = "hola";
+var alimentos = "Carnes";
+var suma = 2+2;
+var fuisteAlGym = false;
+var deportes = [
+    "futbol",
+    "baloncesto",
+    "baseball"
+]
+
+var deportista = {
+    nombre: "Mike",
+    deporte: "Nado sincronizado"
+}
+
+var deportistas = [
+    {
+        nombre: "Mike",
+        deporte: "Nado Sincronizado"
+    },
+    {
+        nombre: "Aztlan",
+        deporte: "Atletismo"
+    }
+]
+//EVENTOS
+document.getElementById("palabraUno").innerHTML = hola
+document.getElementById("palabrados").innerHTML = alimentos
+document.getElementById("palabratres").innerHTML = suma;
+document.getElementById("palabracuatro").innerHTML = fuisteAlGym
+document.getElementById("palabracinco").innerHTML = deportes
+document.getElementById("palabraseis").innerHTML = deportista.deporte
+document.getElementById("palabrasiete").innerHTML = deportistas[1].deporte
+    
+
+
+
+
